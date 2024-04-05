aa120eb27a8e8b62ccd17fc46bcfa6cb5dd81790
loid padre <loidpadre@gmail.com>
2024-04-05 01:04:17 -0300

    margin ajusts

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,16 @@
-import {Handshake} from "lucide-react"
-import {BiLogoWhatsapp} from "react-icons/bi"
-import { Link } from "react-router-dom"
-export const Card = ({casa} ) =>{
-    const {id, image, description, price,localization,cep,rua } = casa
-    return(
-        <div className="w-64 rounded-xl flex flex-col hover:cursor-pointer hover:shadow-lg hover:scale-105 transition duration-200 ease-in-out  gap-5 bg-orange-700 text-white">
-            <img src={image.imagePrincipal} className="w-full h-60 object-cover rounded-ss-xl rounded-se-xl" alt="image" />
+import { Handshake } from "lucide-react";
+import { BiLogoWhatsapp } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+export const Card = ({ casa }) => {
+    const { id, image, description, price, localization, cep, rua } = casa;
+    return (
+        // Aqui, "w-full sm:w-3/4 md:w-64" fará o card ocupar toda a largura em telas muito pequenas,
+        // 3/4 da largura em telas pequenas (sm:) e, finalmente, a largura original de 64 em telas médias para cima.
+        <div className="w-full sm:w-3/4 md:w-64 rounded-xl flex flex-col hover:cursor-pointer hover:shadow-lg hover:scale-105 transition duration-200 ease-in-out gap-5 bg-orange-700 text-white">
+            <img src={image.imagePrincipal} className="w-full h-60 object-cover rounded-t-xl" alt="Imagem da Casa" />
             <div className="p-5">
-                <div className="flex justify-between items-center ">
+                <div className="flex justify-between items-center">
                     <h1 className="text-2xl font-semibold">R$ {price}</h1>
                     <p className="text-md text-orange-700 bg-white p-2 rounded-md border-none">{localization}</p>
                 </div>
@@ -15,16 +18,18 @@ export const Card = ({casa} ) =>{
                     <p className="font-semibold text-xl">Rua: <span className="font-semibold">{rua}</span></p>
                 </div>
                 <div>
-                    <p >Descrição: <span className="text-white/55">{description}</span></p>
+                    <p>Descrição: <span className="text-white/55">{description}</span></p>
                 </div>
-                <div className="flex flex-col justify-center items-end align-bottom mb-0">
-                <button className="mt-4 p-2 rounded-md w-full align-middle bg-zinc-900 text-white"><Link to={`detail/${id}`} >Ver destalhes</Link></button>
-                <div className="mt-2 justify-center p-2 flex gap-5 rounded-md w-full align-middle bg-green-700 text-white">
-                    <BiLogoWhatsapp size={27}/>
-                    <p>+55 8675534322</p>
-                </div>
+                <div className="flex flex-col justify-center items-end mb-0">
+                    <button className="mt-4 p-2 rounded-md w-full bg-zinc-900 text-white">
+                        <Link to={`detail/${id}`}>Ver detalhes</Link>
+                    </button>
+                    <div className="mt-2 justify-center p-2 flex gap-5 rounded-md w-full bg-green-700 text-white">
+                        <BiLogoWhatsapp size={27}/>
+                        <p>+55 8675534322</p>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ export const About = () => {
     return (
         // Alterado de md:flex-col para flex-col para que o layout seja sempre em coluna, e ajustado o espaçamento para telas menores
         <div className="flex flex-col items-center gap-10 mx-4  md:m-52">
-            <img src={logo} alt="Habitue Logo" className="w-1/2 mt-[-6rem] md:w-1/4" /> 
+            <img src={logo} alt="Habitue Logo" className="w-1/2 md:w-1/4" /> 
             <div className="text-center md:text-left"> 
                 <h1 className="text-orange-700 text-4xl md:text-6xl mb-5">Habitue</h1>
                 <p>A Habitue é uma plataforma inovadora e amigável criada pela <span className="text-orange-700">SCORPION COMPANY</span>, dedicada ao aluguel de casas, projetada para conectar proprietários e inquilinos de forma eficiente e segura. Nossa missão é simplificar o processo de locação, tornando-o mais transparente, rápido e sem complicações para todos os envolvidos. Com uma interface intuitiva, a Habitue permite aos usuários navegar facilmente por uma ampla seleção de propriedades, filtrando por localização, preço, número de quartos e outras características essenciais para encontrar a casa perfeita para suas necessidades.</p>
