f09aa166906875236af579dff91779e91f9ef12a
loid padre <loidpadre@gmail.com>
2024-04-05 00:50:09 -0300

    snipper ajusts

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -49,6 +49,11 @@ export const Detail = () => {
             </div>
         </div>
     ) : (
-        <p>Carregando detalhes da casa...</p>
+        <div className="flex gap-2 justify-center items-center text-center mt-40 mb-60">
+                {/* Spinners mantêm o mesmo estilo independente do tamanho da tela */}
+                <div className="w-5 h-5 rounded-full animate-pulse bg-orange-700"></div>
+                <div className="w-5 h-5 rounded-full animate-pulse bg-orange-700"></div>
+                <div className="w-5 h-5 rounded-full animate-pulse bg-orange-700"></div>
+            </div>
     );
 };
